f15c27c22d03703f575ee742c4d6bd72215a7e9e
Aimen <aimenhamed@live.com.au>
2021-06-29 16:01:07 +1000

    added buttons + cleaned up

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,36 @@
 import 'package:flutter/material.dart';
 
-// void main() {
-//   runApp(MyApp());
-// }
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What is your favourite colour?',
+      'What is your favourite animal?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my text.'),
+        body: Column(
+          children: <Widget>[
+            Text('The question:'),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Answer 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
