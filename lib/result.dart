da92f1062d2d7c20272cfddd9c0593c3208c8fe0
Aimen <aimenhamed@live.com.au>
2021-07-01 20:57:16 +1000

    added reset button

--- lib/result.dart
+++ lib/result.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+  final resetHandler;
 
-  Result(this.resultScore);
+  Result(this.resultScore, this.resetHandler);
 
   String get resultPhrase {
     var resultText;
@@ -22,13 +23,29 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(
-          fontSize: 36,
-          fontWeight: FontWeight.bold,
-        ),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          TextButton(
+            child: Text(
+              'Restart Quiz',
+            ),
+            style: TextButton.styleFrom(
+              primary: Colors.blue,
+              textStyle: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            onPressed: resetHandler,
+          ),
+        ],
       ),
     );
   }
